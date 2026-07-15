cb46efcd5429da91313bbfa759c0488459fd9371
jonibek99 <jonibekannaev5@gmail.com>
2026-07-15 16:42:01 +0500

    solo

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -2,6 +2,7 @@ import Siyo_krasofka_part from './Home_Complements/siyo_krasofka_selling'
 import Main_krasofka_module  from './Home_Complements/main_krasofka_part'
 import Rasmlar_part from './Home_Complements/rasmlar'
 import Safet_krasofka_part from './Home_Complements/safet_krasofka'
+import Lorem_part from './Home_Complements/lorem'
 const Home=()=>{
     return (
         <>
@@ -9,6 +10,7 @@ const Home=()=>{
         <Siyo_krasofka_part/>
         <Rasmlar_part/>
         <Safet_krasofka_part/>
+        <Lorem_part/>
         </>
     )
 }
